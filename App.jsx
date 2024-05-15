31e0679839392681559ca923c9aacadcf812e84c
AngeloDomingo <124044199+AngeloDomingo@users.noreply.github.com>
2024-05-16 00:43:43 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+// import Router.................
+import Router from './router/Router'
+
+export default function App() {
+    return(
+      <>
+      <Router/>
+      </>
+    )
+}
+
